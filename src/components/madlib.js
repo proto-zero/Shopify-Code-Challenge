a912f0f6dcfeabf515ebcfb0566a8ba88142e416
Sean Lenhart <theseanlenhart@gmail.com>
2022-05-14 02:02:18 -0400

    functional madlib component

--- src/components/madlib.js
+++ src/components/madlib.js
@@ -3,11 +3,30 @@ import './genie.css';
 
 function Genie() {
     const [userPrompt, setUserPrompt] = useState("");
-    const [promptArray, setPromptArray] = useState(["name a band", "name a book", "name an album"]);
-    const [result, setResult] = useState(["the beatles", "that one time in amsterdam", "metal gear 20th anniversary metal gear music collection"]);
+    const [result1, setResult1] = useState("");
+    const [result2, setResult2] = useState("");
+    const [result3, setResult3] = useState("");
 
-    const data = {
-        prompt: userPrompt,
+    const data1 = {
+        prompt: `Halloween costume from ${userPrompt}`,
+        temperature: 0.5, 
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+    };
+
+    const data2 = {
+        prompt: `Job title of someone who works in ${userPrompt}`,
+        temperature: 0.5, 
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+    };
+
+    const data3 = {
+        prompt: `catchphrase of a ${userPrompt}`,
         temperature: 0.5, 
         max_tokens: 64,
         top_p: 1.0,
@@ -15,7 +34,7 @@ function Genie() {
         presence_penalty: 0.0,
     };
     
-    async function handleSubmit(e) {
+    async function handleSubmit1(e) {
         e.preventDefault();
         const sendToApi = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
             method: "POST",
@@ -23,44 +42,87 @@ function Genie() {
             "Content-Type": "application/json",
             Authorization: `Bearer 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`,
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(data1),
         });
         const response = await sendToApi.json();
-        const newResponse = response.choices[0].text;
-        setResult(result => [...result, newResponse]);
-        setPromptArray(promptArray => [...promptArray, userPrompt])
+        setResult1(response.choices[0].text);
         setUserPrompt("");
     }
 
-    function testyTest() {
-        setPromptArray(promptArray => [...promptArray, userPrompt])
-        setResult(result => [...result, userPrompt]);
+    async function handleSubmit2(e) {
+        e.preventDefault();
+        const sendToApi = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer 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`,
+            },
+            body: JSON.stringify(data2),
+        });
+        const response = await sendToApi.json();
+        setResult2(response.choices[0].text);
+        setUserPrompt("");
     }
 
-    let size = result.length;
-
-    let resultArray = (
-        <div>
-            {promptArray.reverse().map((prompt, i) => (
-                <div>
-                    <div className="prompt">
-                        <p key={prompt}>{prompt}</p>
-                    </div>
-                    <div className="results">
-                        <p key={i}>{result[size - (i + 1)]}</p>
-                    </div>
-                </div>   
-            ))}
-        </div>
-    )
+    async function handleSubmit3(e) {
+        e.preventDefault();
+        const sendToApi = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer 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`,
+            },
+            body: JSON.stringify(data3),
+        });
+        const response = await sendToApi.json();
+        setResult3(response.choices[0].text);
+        setUserPrompt("");
+    }
 
 
     return (
         <div className="main-container">
             <div className="app-container">
-            <div className="title-container">
+                <div className="title-container">
                     <div className="title">
                         <h1>MadLib Generator</h1>
+                    </div>
+                </div>
+                <div className="madlib-container">
+                    <div className="user-input">
+                        USER INPUT
+                        <form onSubmit={handleSubmit1}>
+                            <p>name a time period</p>
+                            <input
+                                type="text"
+                                placeholder="Renaissance"
+                                onChange={(e) => setUserPrompt(e.target.value)}
+                            />
+                            <button>submit</button>
+                        </form>
+                        <form onSubmit={handleSubmit2}>
+                           <p>name a location</p>
+                           <input
+                                type="text"
+                                placeholder="Space"
+                                onChange={(e) => setUserPrompt(e.target.value)}
+                            />
+                            <button>submit</button>
+                        </form>
+                        <form onSubmit={handleSubmit3}>
+                            <p>name an animal</p>
+                            <input
+                                type="text"
+                                placeholder="Cat"
+                                onChange={(e) => setUserPrompt(e.target.value)}
+                            />
+                            <button>submit</button>
+                        </form>
+                    </div>
+                    <div className="results-container">
+                        SCRIPT
+                        <p>Ted: Barney, what did you do with my {result1}</p>
+                        <p>Barney: Ted, you don't need that anymore. Tonight we're dressing up as {result2}s. It's gonna be {result3}!</p>
                     </div>
                 </div>
             </div>
